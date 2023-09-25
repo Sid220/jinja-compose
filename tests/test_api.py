e7f8e3988ae020c4f4f42f5b944574f58f446e0d
The Fake Slim Shady <74916637+Sid220@users.noreply.github.com>
2023-09-25 18:51:52 -0400

    Fix codecov

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,13 +2,13 @@ import argparse
 
 import yaml
 
-from jinja_compose_wrapper import libjinja_compose
+from jinja_compose_wrapper import run_build
 
 
 def test_build():
-    libjinja_compose.run_build(argparse.Namespace(injection_file="tests/test_generation/compose.py",
-                                                  output="tests/test_generation/compose.yaml",
-                                                  template="tests/test_generation/compose.jyml"))
+    run_build(argparse.Namespace(injection_file="tests/test_generation/compose.py",
+                                 output="tests/test_generation/compose.yaml",
+                                 template="tests/test_generation/compose.jyml"))
     with open("tests/test_generation/compose.yaml", "r") as fh:
         with open("tests/test_generation/expected.yaml", "r") as fh2:
             assert yaml.safe_load(fh) == yaml.safe_load(fh2)
